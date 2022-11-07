48b7a25db469b00f68926509bdde158f430bd753
monkuma <kumamoto.jobs@gmail.com>
2022-11-07 22:07:35 +0900

    first

---
+++ src/script.js
@@ -0,0 +1,21 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+async function main() {
+  const newLink = await prisma.link.create({
+    data: {
+      description: "Description udemy",
+      url: "yahoo.co.jp",
+    },
+  });
+  const allLinks = await prisma.link.findMany();
+  console.log(allLinks);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    prisma.$disconnect;
+  });
